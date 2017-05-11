fc69e85fc11d4c0c6b6a03a223e2e770cbc09325
silsuer <1154888444@qq.com>
2017-05-11 23:53:38 +0800

    create a new base controller class for framework. And add the Tag Analysis class here.

---
+++ S/Core/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/11
+ * Time: 22:05
+ */
+
+namespace S;
+
+
+class Controller
+{
+
+}
---
+++ S/Core/S_Tag.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/11
+ * Time: 22:17
+ * 标签类，用于自定义属于自己的标签解析类
+ */
+
+namespace S;
+
+
+class Tag
+{
+
+}
